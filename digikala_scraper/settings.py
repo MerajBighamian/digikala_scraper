431021f8fd72704d6c6219d64256dff18cc542b5
Meraj Bighamian <merajbighamian@gmail.com>
2022-10-03 12:30:37 +0330

    add apis app to setting and restframework

--- digikala_scraper/settings.py
+++ digikala_scraper/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apis.apps.ApisConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
