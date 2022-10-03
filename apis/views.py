ac335495985d87323b3e8a4199caf3aaf12a2e9a
Meraj Bighamian <merajbighamian@gmail.com>
2022-10-03 12:42:54 +0330

    import libraries to project

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .serializer import UrlSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Urls, Products
+from .scraper import get_products
+import json
+from rest_framework.exceptions import ValidationError
